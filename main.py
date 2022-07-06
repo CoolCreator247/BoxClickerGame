0ba7382cb5dfb3f30a5d16e0983cc76268a0b57c
Faris Allafi <96661970+CoolCreator247@users.noreply.github.com>
2022-07-06 13:35:39 -0700

    does not work, but no errors

--- main.py
+++ main.py
@@ -6,47 +6,49 @@ import time
 # create root window
 root = Tk()
 
+# def functions
+def RunCode1():
+    bg = "black"
+    time.sleep(2)
+    bg = "blue"
+    time.sleep(2)
+    bg = "black"
+def RunCode2():
+    bg = "black"
+    time.sleep(2)
+    bg = "green"
+    time.sleep(2)
+    bg = "black"
+
+def RunCode3():
+    bg = "black"
+    time.sleep(2)
+    bg = "red"
+    time.sleep(2)
+    bg = "black"
+def RunCode4():
+    bg = "black"
+    time.sleep(2)
+    bg = "yelow"
+    time.sleep(2)
+    bg = "black"
 def RunCode():
     list1 = [1, 2, 3, 4]
     for x in range (1,4):
         TheRandomChoice = random.choice(list1)
         if TheRandomChoice == 1:
-            def RunCode1():
-                bg = "black"
-                time.sleep(2)
-                bg = "blue"
-                time.sleep(2)
-                bg = "black"
-        if TheRandomChoice == 2:
-            def RunCode2():
-                bg = "black"
-                time.sleep(2)
-                bg = "green"
-                time.sleep(2)
-                bg = "black" 
-        if TheRandomChoice == 3:
-            def RunCode3():
-                bg = "black"
-                time.sleep(2)
-                bg = "red"
-                time.sleep(2)
-                bg = "black"
-        if TheRandomChoice == 4:
-            def RunCode4():
-                bg = "black"
-                time.sleep(2)
-                bg = "yelow"
-                time.sleep(2)
-                bg = "black"                       
+            btn = Button(root, text="1",fg = "black", command=RunCode1)  
 
-    btn = Button(root, text="1",
-                  command=RunCode1)
-    btn = Button(root, text="2",
-                 fg="black", command=RunCode2)
-    btn = Button(root, text="3",
-                 fg="black", command=RunCode3)
-    btn = Button(root, text="4",
-                 fg="black", command=RunCode4)
+        if TheRandomChoice == 2:
+            btn = Button(root, text="2",fg="black", command=RunCode2)         
+
+        if TheRandomChoice == 3:
+            btn = Button(root, text="3",fg="black", command=RunCode3)
+
+        if TheRandomChoice == 4:
+            btn = Button(root, text="4",fg="black", command=RunCode4)
+            
+                                   
 # root window title and dimension
 root.title("Box Clicker")
 
