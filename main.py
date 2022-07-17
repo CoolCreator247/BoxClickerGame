1ca38eb7fdd01cd0436ed0e9b859010b91d4b270
Faris Allafi <96661970+CoolCreator247@users.noreply.github.com>
2022-07-17 08:36:59 -0700

    more errors

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def RunCode1():
     bg = "blue"
     time.sleep(2)
     bg = "black"
+  
 def RunCode2():
     bg = "black"
     time.sleep(2)
@@ -23,30 +24,54 @@ def RunCode2():
 def RunCode3():
     bg = "black"
     time.sleep(2)
-    bg = "red"
+    bg = "white"
     time.sleep(2)
     bg = "black"
+  
 def RunCode4():
     bg = "black"
     time.sleep(2)
     bg = "yelow"
     time.sleep(2)
     bg = "black"
+
+  
 def RunCode():
     list1 = [1, 2, 3, 4]
     for x in range (1,4):
         TheRandomChoice = random.choice(list1)
         if TheRandomChoice == 1:
-            btn = Button(root, text="1",fg = "black", command=RunCode1)  
+            frame = Frame(root)
+            frame.pack()
+            bottomframe = Frame(root)
+            bottomframe.pack( side = BOTTOM )
+            redbutton = Button(frame, text = '1', fg = "black",                      command=RunCode1)   
+            redbutton.pack( side = LEFT)           
 
         if TheRandomChoice == 2:
-            btn = Button(root, text="2",fg="black", command=RunCode2)         
+          frame = Frame(root)
+          frame.pack()
+          bottomframe = Frame(root)
+          bottomframe.pack( side = BOTTOM )
+          greenbutton = Button(frame, text = '2', fg = "black",            command=RunCode2)
+          greenbutton.pack( side = LEFT )
 
         if TheRandomChoice == 3:
-            btn = Button(root, text="3",fg="black", command=RunCode3)
+          frame = Frame(root)
+          frame.pack()
+          bottomframe = Frame(root)
+          bottomframe.pack( side = BOTTOM )
+          bluebutton = Button(frame, text ='3', fg = "black", command=RunCode3)
+bluebutton.pack( side = LEFT )
 
         if TheRandomChoice == 4:
-            btn = Button(root, text="4",fg="black", command=RunCode4)
+          frame = Frame(root)
+          frame.pack()
+          bottomframe = Frame(root)
+          bottomframe.pack( side = BOTTOM )
+            blackbutton = Button(bottomframe, text ='4', fg ='black', command=RunCode4)
+blackbutton.pack( side = BOTTOM)
+
             
                                    
 # root window title and dimension
@@ -62,3 +87,4 @@ btn = Button(root, text="Click to start",
 btn.grid(column=1, row=0)
 
 root.mainloop()
+
